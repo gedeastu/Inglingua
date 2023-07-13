16091a5b8fdb7cb7121c0edc827f561ae792ba37
gedeastu <gede.astu@gmail.com>
2023-07-13 13:27:57 +0700

    up

--- main/src/components/teacherSection/main.jsx
+++ main/src/components/teacherSection/main.jsx
@@ -1,21 +1,40 @@
+import './main.css'
+import iconProfile from './img/Ellipse 37.svg'
 export default function Main(){
-    const elements = [
-        <div>
-        1
-        </div>,
-        <div>
-        2
-        </div>
-    ]  
+    const TimeLoop = 4;
+
+    const ListTeacher = () => {
+        return(
+            <>
+            <div className='relative z-50 flex flex-col mx-auto items-center gap-y-2 w-[28rem]'>
+            <div id="Teacher" className='relative z-50 p-0.5 flex items-center w-full h-max mx-auto rounded-2xl bg-gradient-to-r from-[#172A87] to-[#E05E0A]'>
+            <div className="bg-black w-full h-full rounded-2xl px-5 py-3">
+            <header className='flex flex-row items-center gap-3'>
+            <img src={iconProfile} alt="" />
+            <article className='text-white'>
+            <h1>Mr. Pudidi</h1>
+            <h2>Umur : 35</h2>
+            </article>
+            </header>
+            <main className='flex flex-row items-end justify-between pt-5'>
+            <article className='text-white'>
+            <h1>Edukasi</h1>
+            <p>S.Pd di Bahasa Inggris, M.A. in English</p>
+            </article>
+            <button className='text-white/50'>
+             More
+            </button>
+            </main>
+            </div>
+            </div>
+            </div>
+            </>
+        )
+    }
     return(
         <>
-        <div>
-           {
-            elements.map((elements, index)=>(
-                <div key={index}>
-                    {elements}
-                </div>
-            ))} 
+        <div className="flex flex-col justify-evenly h-[45rem]">
+         {Array(TimeLoop).fill(ListTeacher())}
         </div>
         </>
     )
--- main/src/pages/teacherSection.jsx
+++ main/src/pages/teacherSection.jsx
@@ -1,10 +1,16 @@
 import './teacherSection.css'
 import Header from '../components/teacherSection/header'
+import Main from '../components/teacherSection/main'
 export default function TeacherSection(){
     return (
         <>
         <div className='bg-black relative h-screen max-w-full'>
+        <header>
         <Header/>
+        </header>
+        <main className=''>
+        <Main/>
+        </main>
         <div id="circleRed" className='bg-[#D35A0B] absolute w-52 h-72 blur-[10rem] rounded-full -left-40 -top-10'></div>
         <div id="circleBlue" className='bg-[#172A87] absolute w-52 h-72 blur-[10rem] rounded-full right-0 -top-10'></div>
         </div>
